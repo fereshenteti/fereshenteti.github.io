db0d6f481263bff039c0f9d6a8cada12ca980e0e
Feres Henteti <fereshenteti@gmail.com>
2026-05-10 05:00:57 +0200

    updated my image sequence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ const Home = () => {
       <ToolsCloud />
       <ProcessSection />
 
-      {/* <ImageSequenceSection /> */}
+      <ImageSequenceSection />
       <TestimonialsSection />
       <FinalCTA />
 
